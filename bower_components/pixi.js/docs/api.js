ba3fd48ea3c21183ec33b0c7023952b1b5fac9b0
BlueInt32 <simon.budin@gmail.com>
2014-03-29 22:41:32 +0100

    adding pixi.js and usual stuff

---
+++ bower_components/pixi.js/docs/api.js
@@ -0,0 +1,62 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "AbstractFilter",
+        "AlphaMaskFilter",
+        "AssetLoader",
+        "AtlasLoader",
+        "BaseTexture",
+        "BitmapFontLoader",
+        "BitmapText",
+        "BlurFilter",
+        "CanvasGraphics",
+        "CanvasRenderer",
+        "Circle",
+        "ColorMatrixFilter",
+        "ColorStepFilter",
+        "DisplacementFilter",
+        "DisplayObject",
+        "DisplayObjectContainer",
+        "DotScreenFilter",
+        "Ellipse",
+        "EventTarget",
+        "Graphics",
+        "GrayFilter",
+        "ImageLoader",
+        "InvertFilter",
+        "JsonLoader",
+        "MovieClip",
+        "PIXI.PixiFastShader",
+        "PIXI.PixiShader",
+        "PixelateFilter",
+        "Point",
+        "PolyK.AjaxRequest",
+        "PolyK.InteractionData",
+        "PolyK.InteractionManager",
+        "PolyK._PointInTriangle",
+        "PolyK._convex",
+        "Polygon",
+        "Rectangle",
+        "RenderTexture",
+        "SepiaFilter",
+        "Spine",
+        "Sprite",
+        "SpriteSheetLoader",
+        "Stage",
+        "Text",
+        "Texture",
+        "TilingSprite",
+        "TwistFilter",
+        "WebGLRenderer"
+    ],
+    "modules": [
+        "PIXI"
+    ],
+    "allModules": [
+        {
+            "displayName": "PIXI",
+            "name": "PIXI"
+        }
+    ]
+} };
+});
