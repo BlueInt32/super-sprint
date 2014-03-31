0777beb55e48b7f7d18a7886c391b16cd128a914
BlueInt32 <simon.budin@gmail.com>
2014-04-01 00:28:38 +0200

    added class car, that's a start !

---
+++ app/js/Car.js
@@ -0,0 +1,22 @@
+﻿'use strict';
+
+function Car(color)
+{
+	this.color = color;
+	console.log('car instantiated');
+}
+
+Car.prototype.color = '#00000';
+
+Car.prototype.sayColor = function()
+{
+	console.log("I'm "+ this.color);
+};
+
+
+
+var redCar = new Car('#F00');
+var carColorTeller = redCar.sayColor;
+
+redCar.sayColor();
+carColorTeller.call(redCar);
