472cfef632581c329169d2c794f63858713e4a95
BlueInt32 <budsi_fr@hotmail.com>
2014-04-03 18:08:49 +0200

    tests matter js

--- app/js/Game.js
+++ app/js/Game.js
@@ -1,40 +1,22 @@
 ﻿function Game()
 {
 	var self = this;
+	this.Load = function ()
+	{
 
 
-	/// MATTER
-	var Engine = Matter.Engine,
-		World = Matter.World,
-		Body = Matter.Body,
-		Bodies = Matter.Bodies,
-		Constraint = Matter.Constraint,
-		Composites = Matter.Composites,
-		MouseConstraint = Matter.MouseConstraint;
+		/// PIXI
 
+		const STAGE_WIDTH = window.innerWidth, STAGE_HEIGHT = window.innerHeight;
+		self.stage = new PIXI.Stage(0xFFFFFF); // create an new instance of a pixi stage
+		self.renderer = PIXI.autoDetectRenderer(STAGE_WIDTH, STAGE_HEIGHT, null, false, true); // create a renderer instance.
+		document.getElementById('game').appendChild(self.renderer.view);// add the renderer view element to the DOM
 
-	// create a Matter.js engine
-	var engine = Engine.create(document.getElementById('game'), {
-		render: {
-			options: {
-				wireframes: false,
-				background: 'http://brm.io/matter-js-demo/img/wall-bg.jpg'
-			}
-		}
-	});
-
-
-	/// PIXI
-
-	const STAGE_WIDTH = window.innerWidth, STAGE_HEIGHT = window.innerHeight;
-	this.stage = new PIXI.Stage(0xFFFFFF); // create an new instance of a pixi stage
-	this.renderer = PIXI.autoDetectRenderer(STAGE_WIDTH, STAGE_HEIGHT, null, false, true); // create a renderer instance.
-	document.getElementById('game').appendChild(this.renderer.view);// add the renderer view element to the DOM
-
-	console.log(engine.render);
-	console.log(this.renderer);
-
-	this.animate = function()
+		const loader = new PIXI.AssetLoader(["content/images/car.png"]);
+		loader.onComplete = self.AssetsLoaded;
+		loader.load();
+	}
+	this.animate = function ()
 	{
 		if (self.keyboardHandler.Keys.accelerate)
 		{
@@ -58,28 +40,76 @@
 		}
 		self.car.updateData();
 		requestAnimFrame(self.animate);
-		
+
 		self.log("speed", self.car.speedValue);
 		self.log("rotation", self.car.rotation);
 		self.renderer.render(self.stage);
 	};
+	this.AssetsLoaded = function ()
+	{
 
-	requestAnimFrame(this.animate);
+		/// MATTER WORLD CREATION
+		var Engine = Matter.Engine,
+			World = Matter.World,
+			Body = Matter.Body,
+			Bodies = Matter.Bodies,
+			Constraint = Matter.Constraint,
+			Composites = Matter.Composites,
+			MouseConstraint = Matter.MouseConstraint;
 
-	// create a texture from an image path
-	this.carTexture = PIXI.Texture.fromImage("content/images/car.png");
-	// create a new Sprite using the texture
-	this.car = new Car(this.carTexture);
-	this.stage.addChild(this.car);
+		// create a Matter.js engine
+		var engine = Engine.create(document.getElementById('game'), {
+			//render: 
+			//{
+			//	options: {
+			//		wireframes: false,
+			//		background: 'http://brm.io/matter-js-demo/img/wall-bg.jpg'
+			//	}
+			//}
+		});
 
-	this.keyboardHandler = new KeyboardHandler();
-	document.onkeydown = this.keyboardHandler.HandleKeyDown.bind(this.keyboardHandler);
-	document.onkeyup = this.keyboardHandler.HandleKeyUp.bind(this.keyboardHandler);
+		// Create walls
+		var offset = 10,
+			wallOptions = {
+				isStatic: true,
+				render: {
+					visible: true
+				}
+			};
 
-	this.log = function(elId, val)
+		// add some invisible some walls to the world
+		World.add(engine.world,
+		[
+			Bodies.rectangle(400, -offset, 800 + 2 * offset, 50, wallOptions),
+			Bodies.rectangle(400, 600 + offset, 800 + 2 * offset, 50, wallOptions),
+			Bodies.rectangle(800 + offset, 300, 50, 600 + 2 * offset, wallOptions),
+			Bodies.rectangle(-offset, 300, 50, 600 + 2 * offset, wallOptions)
+		]);
+
+
+
+		requestAnimFrame(self.animate);
+
+		// create a texture from an image path
+		self.carTexture = PIXI.Texture.fromFrame("content/images/car.png");
+		// create a new Sprite using the texture
+		self.car = new Car(self.carTexture);
+		self.stage.addChild(self.car);
+
+		self.keyboardHandler = new KeyboardHandler();
+		document.onkeydown = self.keyboardHandler.HandleKeyDown.bind(self.keyboardHandler);
+		document.onkeyup = self.keyboardHandler.HandleKeyUp.bind(self.keyboardHandler);
+
+		this.log = function (elId, val)
+		{
+			document.getElementById(elId).innerHTML = val;
+		};
+	}
+	this.log = function (elId, val)
 	{
 		document.getElementById(elId).innerHTML = val;
 	};
 }
 
-var superSprint = new Game();
+var superSprintGame = new Game();
+superSprintGame.Load();
