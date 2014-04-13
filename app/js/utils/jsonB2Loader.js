55a87a690ae720bf34f54694c687c0fac46d45d8
BlueInt32 <simon.budin@gmail.com>
2014-04-13 10:59:56 +0200

    non colliding tracks data : SetAsArray wasn't called correctly

--- app/js/utils/jsonB2Loader.js
+++ app/js/utils/jsonB2Loader.js
@@ -3,13 +3,15 @@ function jsonB2Loader(trackPath, consts, world)
 	var me = this;
 	this.trackData = null;
 	this.world = world;
+	this.consts = consts;
 	this.loadJSON(trackPath	, function(response)
 	{
 		jsonresponse = JSON.parse(response);
 		me.trackData = jsonresponse;
 
-		me.setBodies(jsonresponse.box2d, consts.STAGE_WIDTH_B2, consts.STAGE_HEIGHT_B2);
+		me.setBodies(jsonresponse.box2d, me.consts.STAGE_WIDTH_B2, me.consts.STAGE_HEIGHT_B2);
 	});
+		
 };
 
 
@@ -24,45 +26,53 @@ jsonB2Loader.prototype.debugDraw = function()
 	debugDrawer.SetFlags(
 		b2.dyn.b2DebugDraw.e_shapeBit 
 		| b2.dyn.b2DebugDraw.e_jointBit 
-		//| b2.dyn.b2DebugDraw.e_centerOfMassBit 
+		| b2.dyn.b2DebugDraw.e_centerOfMassBit 
 		//| b2.dyn.b2DebugDraw.e_aabbBit
 	);
 	this.world.SetDebugDraw(debugDrawer);
 };
 
-jsonB2Loader.prototype.setBodies = function(bodyEntities, canvasWidth, canvasHeight) 
+jsonB2Loader.prototype.setBodies = function(bodyEntities) 
 {
 	this.bodyDef = new b2.dyn.b2BodyDef();
-	this.bodyDef.type = b2.dyn.b2Body.b2_staticBody;
+	this.fixDef = new b2.dyn.b2FixtureDef();
+	this.fixDef.shape = new b2.shapes.b2PolygonShape();
 
 	for(var i = 0, l = bodyEntities.bodies.body.length; i < l; i++)
 	{
-		var entity = bodyEntities.bodies.body[i];
-		this.bodyDef.position.Set(entity.x/100 + canvasWidth / 2, entity.y/-100  + canvasHeight / 2);
-		
-	    this.bodyDef.userData = entity.name + i;
-
-	    //console.log(entity.fixtures.fixture);
-	    for(var j = 0, m = entity.fixtures.fixture.length; j < m; j++) 
+		var jsonBody = bodyEntities.bodies.body[i];
+		this.bodyDef.position.Set(jsonBody.x/100 + this.consts.STAGE_WIDTH_B2 / 2, jsonBody.y/-100  + this.consts.STAGE_HEIGHT_B2 / 2);
+		switch	(jsonBody.type)
 		{
-	    	var body = this.world.CreateBody(this.bodyDef);
-			var fixture = entity.fixtures.fixture[j];
-			//console.log(fixture);
-			this.fixDef = new b2.dyn.b2FixtureDef();
-			this.fixDef.shape = new b2.shapes.b2PolygonShape();
+			case "dynamic":
+			this.bodyDef.type = b2.dyn.b2Body.b2_dynamicBody; break;
+			case "static":
+			this.bodyDef.type = b2.dyn.b2Body.b2_staticBody; break;
+			default:
+			this.bodyDef.type = b2.dyn.b2Body.b2_staticBody; break;
+		}
+		
+	    this.bodyDef.userData = jsonBody.name + i;
+	    var body = this.world.CreateBody(this.bodyDef);
+
+	    for(var j = 0, m = jsonBody.fixtures.fixture.length; j < m; j++) 
+		{
+			var fixture = jsonBody.fixtures.fixture[j];
+			this.fixDef.isSensor = false;
 			this.fixDef.density = 1;
-			this.fixDef.restitution = 0.4;
+			this.fixDef.restitution = 0.1;
+
+			var vertices = [];
 			for (var k = fixture.vertex.length - 1; k >= 0; k--) {
-				fixture.vertex[k].x /= 100; 
-				fixture.vertex[k].y /= -100;
+				vertices.push(new b2.cMath.b2Vec2(fixture.vertex[k].x / 100,fixture.vertex[k].y / -100));
 			};
-			this.fixDef.shape.SetAsArray(fixture.vertex, fixture.vertex.length);
-			//var arrayVertices = fixture.vertex;
-			var fix = body.CreateFixture(this.fixDef);
-			//console.log(fix);
+
+			this.fixDef.shape.SetAsArray(vertices, 3);
+			body.CreateFixture(this.fixDef, 0);
+			body.SetUserData("trackData "+ i + " " + j);
 		}
 	}
-	this.ready = true;
+	//console.log(this.world.simon);
 };
 
 jsonB2Loader.prototype.loadJSON = function (filePath, callback) 
