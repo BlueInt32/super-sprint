4810404c7b4104c0f292c542af868722db52ac1b
BlueInt32 <simon.budin@gmail.com>
2014-04-30 00:02:18 +0200

    commented

--- app/js/ContactManager.js
+++ app/js/ContactManager.js
@@ -1,5 +1,9 @@
 
-/// this class encapsulates the management of box2d contacts
+/**
+ * this class encapsulates the management of box2d contacts
+ * @param {b2World} world in which the bodies evolve
+ * @param {Cars[]} cars
+ */
 var ContactManager = function(world, cars)
 {
     var me = this;
@@ -18,9 +22,11 @@ var ContactManager = function(world, cars)
 
 };
 
-
-/// this determines what kind of contact has been detected
-/// contacts are established by fixture names
+/**
+ * This determines what kind of contact has been detected
+ * contacts are established by fixture names
+ * @param {[b2Contact]} contact
+ */
 ContactManager.prototype.ExtractContactType = function(contact)
 {
     aData = contact.GetFixtureA();
@@ -40,19 +46,31 @@ ContactManager.prototype.ExtractContactType = function(contact)
     return { 'type': '' }
 };
 
-
+/**
+ * Given a b2Contact and the boolean wheter it began or ended, it is the main manager logic.
+ * @param {[b2Contact]} contact
+ * @param {[bool]} began
+ */
 ContactManager.prototype.HandleContact = function(contact, began)
 {
-    var contactInfo = this.ExtractContactType(contact);
-    if(contactInfo.type === "wall")
+    var cInfo = this.ExtractContactType(contact);
+    if(cInfo.type === "wall")
         return;
     if(began)
     {
-        switch(contactInfo.type)
+        switch(cInfo.type)
         {
-            case "cp": this.cars[0].checkPointManager.Step(parseInt(contactInfo.id)); break;
-            case "puddle":this.cars[0].adherence = false; this.cars[0].paddleEffect = puddleRandomDirectionArray[Math.floor(Math.random()*2)];break;
-            case "boost": var boostVector = new b2.cMath.b2Vec2(contactInfo.boostVector.x, contactInfo.boostVector.y);  this.cars[0].ApplyImpulse(boostVector); break;
+            case "cp":
+                this.cars[0].checkPointManager.Step(parseInt(cInfo.id));
+                break;
+            case "puddle":
+                this.cars[0].adherence = false;
+                this.cars[0].paddleEffect = puddleRandomDirectionArray[Math.floor(Math.random()*2)];
+                break;
+            case "boost":
+                var boostVector = new b2.cMath.b2Vec2(cInfo.boostVector.x, cInfo.boostVector.y);
+                this.cars[0].ApplyImpulse(boostVector);
+                break;
         }
     }
     else
