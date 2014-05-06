07c36b3b18bc0eabccf304e07e180f67be096219
BlueInt32 <budsi_fr@hotmail.com>
2014-05-06 18:14:21 +0200

    recreated Car.js in coffee

--- app/js/coffee/car.coffee
+++ app/js/coffee/car.coffee
@@ -1,18 +1,74 @@
 #Car class
 class Car
-    constructor: (@configuration) ->
+	constructor: (@configuration) ->
 
-    setBox2dData: (box2dData) ->
-        @rearTires = box2dData.rearTires
-        @frontTires = box2dData.frontTires
-        @tires = @rearTires.concat(@frontTires)
-        @tiresCount = @tires.length
-        @directionJoints = box2dData.directionJoints
-        @b2Body = box2dData.carBody
+	setBox2dData: (box2dData) ->
+		@rearTires = box2dData.rearTires
+		@frontTires = box2dData.frontTires
+		@tires = @rearTires.concat(@frontTires)
+		@tiresCount = @tires.length
+		@directionJoints = box2dData.directionJoints
+		@b2Body = box2dData.carBody
 
-    setPosition:(chosenPosition) ->
-        temp = chosenPosition.Copy()
-        temp.Add(@b2Body.GetPosition())
-        @b2Body.SetPosition(temp)
+	setPosition:(chosenPosition) ->
+		temp = chosenPosition.Copy()
+		temp.Add(@b2Body.GetPosition())
+		@b2Body.SetPosition(temp)
 
-    coucou: ->
+	updateData:(keyboardData) ->
+		@localAccelerationVector = new b2.math.b2Vec2(0, -@accelerationFactor)
+
+		for i in [0...@tiresCount]
+			@linearVelocities[i] = getLinearVelocity(i);
+			@currentRightForwards[i] = @tires[i].GetWorldVector(new b2.cMath.b2Vec2(0, 1));
+			@vCurrentRightNormals[i] = getLateralVelocity(i);
+
+	getLateralVelocity:(tireIndex) ->
+		currentRightNormal = @tires[tireIndex].GetWorldVector(@LocalNormalVector);
+		vCurrentRightNormal = b2.math.MulFV(
+			b2.math.Dot(currentRightNormal, @linearVelocities[tireIndex]),
+			currentRightNormal
+		);
+		return vCurrentRightNormal;
+
+	getLinearVelocity: (tireIndex) ->
+		return @tires[tireIndex].GetLinearVelocity();
+
+	getForwardVelocity: (tireIndex) ->
+		vCurrentRightForward = b2.math.MulFV(
+			b2.math.Dot(this.currentRightForwards[tireIndex], @linearVelocities[tireIndex]),
+			@currentRightForwards[tireIndex]
+		);
+		return vCurrentRightForward;
+	applyImpulse: (vec2) ->
+		for i in [0...tiresCount]
+			if @adherence
+				tireType = b2.findCustomPropertyValue(@tires[i], 'category', 'string')
+				if tireType == 'wheel_rear' and @drifting
+					@adherenceFactor = 0.2
+				else
+					@adherenceFactor = 1
+
+				impulse = b2.math.MulFV(
+					- this.adherenceFactor * this.tires[i].GetMass(),
+					this.vCurrentRightNormals[i]
+				);
+				if impulse.Length() > @Drift_trigger
+					impulse  = b2.math.MulFV(@Drift_trigger / impulse.Length(), impulse);
+
+				@tires[i].ApplyImpulse(impulse, @tires[i].GetWorldCenter());
+
+			# what does this do ???
+			inertia = @tires[i].GetInertia()
+			vel = @tires[i].GetAngularVelocity();
+			@tires[i].ApplyAngularImpulse(10 * inertia * -vel);
+
+			# natural friction against movement. This is a F = -kv type force.
+			currentForwardNormal = this.GetForwardVelocity(i);
+			currentForwardSpeed = currentForwardNormal.Normalize();
+			dragForceMagnitude = -this.configuration.natural_deceleration * currentForwardSpeed;
+			this.tires[i].ApplyForce( b2.math.MulFV(dragForceMagnitude, currentForwardNormal), this.tires[i].GetWorldCenter() );
+
+			# here we update how the car behave when its puddleEffect is on (sliding on a paddle).
+			if this.puddleEffect != 0
+				this.tires[i].ApplyTorque(this.puddleEffect * this.configuration.puddleFactor);
--- app/js/coffee/compiled/car.js
+++ app/js/coffee/compiled/car.js
@@ -22,7 +22,67 @@
       return this.b2Body.SetPosition(temp);
     };
 
-    Car.prototype.coucou = function() {};
+    Car.prototype.updateData = function(keyboardData) {
+      var i, _i, _ref, _results;
+      this.localAccelerationVector = new b2.math.b2Vec2(0, -this.accelerationFactor);
+      _results = [];
+      for (i = _i = 0, _ref = this.tiresCount; 0 <= _ref ? _i < _ref : _i > _ref; i = 0 <= _ref ? ++_i : --_i) {
+        this.linearVelocities[i] = getLinearVelocity(i);
+        this.currentRightForwards[i] = this.tires[i].GetWorldVector(new b2.cMath.b2Vec2(0, 1));
+        _results.push(this.vCurrentRightNormals[i] = getLateralVelocity(i));
+      }
+      return _results;
+    };
+
+    Car.prototype.getLateralVelocity = function(tireIndex) {
+      var currentRightNormal, vCurrentRightNormal;
+      currentRightNormal = this.tires[tireIndex].GetWorldVector(this.LocalNormalVector);
+      vCurrentRightNormal = b2.math.MulFV(b2.math.Dot(currentRightNormal, this.linearVelocities[tireIndex]), currentRightNormal);
+      return vCurrentRightNormal;
+    };
+
+    Car.prototype.getLinearVelocity = function(tireIndex) {
+      return this.tires[tireIndex].GetLinearVelocity();
+    };
+
+    Car.prototype.getForwardVelocity = function(tireIndex) {
+      var vCurrentRightForward;
+      vCurrentRightForward = b2.math.MulFV(b2.math.Dot(this.currentRightForwards[tireIndex], this.linearVelocities[tireIndex]), this.currentRightForwards[tireIndex]);
+      return vCurrentRightForward;
+    };
+
+    Car.prototype.applyImpulse = function(vec2) {
+      var currentForwardNormal, currentForwardSpeed, dragForceMagnitude, i, impulse, inertia, tireType, vel, _i, _results;
+      _results = [];
+      for (i = _i = 0; 0 <= tiresCount ? _i < tiresCount : _i > tiresCount; i = 0 <= tiresCount ? ++_i : --_i) {
+        if (this.adherence) {
+          tireType = b2.findCustomPropertyValue(this.tires[i], 'category', 'string');
+          if (tireType === 'wheel_rear' && this.drifting) {
+            this.adherenceFactor = 0.2;
+          } else {
+            this.adherenceFactor = 1;
+          }
+          impulse = b2.math.MulFV(-this.adherenceFactor * this.tires[i].GetMass(), this.vCurrentRightNormals[i]);
+          if (impulse.Length() > this.Drift_trigger) {
+            impulse = b2.math.MulFV(this.Drift_trigger / impulse.Length(), impulse);
+          }
+          this.tires[i].ApplyImpulse(impulse, this.tires[i].GetWorldCenter());
+        }
+        inertia = this.tires[i].GetInertia();
+        vel = this.tires[i].GetAngularVelocity();
+        this.tires[i].ApplyAngularImpulse(10 * inertia * -vel);
+        currentForwardNormal = this.GetForwardVelocity(i);
+        currentForwardSpeed = currentForwardNormal.Normalize();
+        dragForceMagnitude = -this.configuration.natural_deceleration * currentForwardSpeed;
+        this.tires[i].ApplyForce(b2.math.MulFV(dragForceMagnitude, currentForwardNormal), this.tires[i].GetWorldCenter());
+        if (this.puddleEffect !== 0) {
+          _results.push(this.tires[i].ApplyTorque(this.puddleEffect * this.configuration.puddleFactor));
+        } else {
+          _results.push(void 0);
+        }
+      }
+      return _results;
+    };
 
     return Car;
 
